4f742635a97c538b83f95ae64b173d2a390dd532
Md.Taohidul Islam <67187868+tao73bot@users.noreply.github.com>
2023-08-07 03:40:49 +0600

    adding drawable methods

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -48,6 +48,66 @@ public class SnakeGame extends JPanel implements KeyListener, ActionListener {
         food = new Point(x, y);
     }
 
+    private void drawTile(Graphics g, int x, int y, Color color) {
+        g.setColor(color);
+        g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+
+    private void drawSnake(Graphics g) {
+        for (int i = 0; i < snake.size(); i++) {
+            Point p = snake.get(i);
+            Color color = (i == 0) ? Color.GREEN : Color.YELLOW; // Head is green, body is yellow
+            drawTile(g, p.x, p.y, color);
+            // Draw eyes on the snake's head
+            if (i == 0) {
+                g.setColor(Color.BLACK);
+                switch (direction) {
+                    case 'U':
+                        g.fillOval(p.x * TILE_SIZE + 5, p.y * TILE_SIZE + 5, 5, 5);
+                        g.fillOval(p.x * TILE_SIZE + 10, p.y * TILE_SIZE + 5, 5, 5);
+                        break;
+                    case 'D':
+                        g.fillOval(p.x * TILE_SIZE + 5, p.y * TILE_SIZE + 10, 5, 5);
+                        g.fillOval(p.x * TILE_SIZE + 10, p.y * TILE_SIZE + 10, 5, 5);
+                        break;
+                    case 'L':
+                        g.fillOval(p.x * TILE_SIZE + 5, p.y * TILE_SIZE + 5, 5, 5);
+                        g.fillOval(p.x * TILE_SIZE + 5, p.y * TILE_SIZE + 10, 5, 5);
+                        break;
+                    case 'R':
+                        g.fillOval(p.x * TILE_SIZE + 10, p.y * TILE_SIZE + 5, 5, 5);
+                        g.fillOval(p.x * TILE_SIZE + 10, p.y * TILE_SIZE + 10, 5, 5);
+                        break;
+                }
+            }
+        }
+    }
+
+    private void drawFood(Graphics g) {
+        drawTile(g, food.x, food.y, Color.RED);
+    }
+
+    private void drawScore(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.PLAIN, 20));
+        g.drawString("Score: " + score, 10, 30);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawSnake(g);
+        drawFood(g);
+        drawScore(g);
+
+        if (gameOver) {
+            g.setColor(Color.RED);
+            g.setFont(new Font("Arial", Font.BOLD, 40));
+            String gameOverMessage = "Game Over!";
+            int messageWidth = g.getFontMetrics().stringWidth(gameOverMessage);
+            g.drawString(gameOverMessage, (TILE_SIZE * BOARD_SIZE - messageWidth) / 2, TILE_SIZE * BOARD_SIZE / 2);
+        }
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Survival of Snake");
