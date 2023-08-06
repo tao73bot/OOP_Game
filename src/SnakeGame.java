86c2940ab84d4bcf19510f12ffc70b99ce67232e
Md.Taohidul Islam <67187868+tao73bot@users.noreply.github.com>
2023-08-07 01:08:52 +0600

    adding food place in random

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SnakeGame extends JPanel implements KeyListener, ActionListener {
     private final int TILE_SIZE = 20;
@@ -16,7 +19,33 @@ public class SnakeGame extends JPanel implements KeyListener, ActionListener {
     private int delay;
 
     public SnakeGame(){
+        setPreferredSize(new Dimension(TILE_SIZE * BOARD_SIZE, TILE_SIZE * BOARD_SIZE));
+        setBackground(Color.BLACK);
+        setFocusable(true);
+        addKeyListener(this);
 
+        snake = new ArrayList<>();
+        snake.add(new Point(10, 10)); // Initial position of the snake
+        direction = 'R'; // Start moving to the right
+        gameOver = false;
+        score = 0;
+        delay = 200;
+
+        spawnFood();
+
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    private void spawnFood() {
+        Random random = new Random();
+        int x, y;
+        do {
+            x = random.nextInt(BOARD_SIZE);
+            y = random.nextInt(BOARD_SIZE);
+        } while (snake.contains(new Point(x, y)));
+
+        food = new Point(x, y);
     }
 
 
@@ -27,5 +56,25 @@ public class SnakeGame extends JPanel implements KeyListener, ActionListener {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
